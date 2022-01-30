fb3e38d7a64dcb1797d0931201d25f8171ed738d
“Omprakash <omprakash.medhe@gmail.com>
2022-01-30 21:36:45 +0530

    forth commit

---
+++ src/test/java/com/osm/APITestingFramework/rough/TestIP.java
@@ -0,0 +1,18 @@
+package com.osm.APITestingFramework.rough;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class TestIP {
+
+	public static void main(String[] args) throws UnknownHostException {
+		 
+		
+		 
+		
+		System.out.println(InetAddress.getLocalHost().getHostAddress());
+		
+
+	}
+
+}
