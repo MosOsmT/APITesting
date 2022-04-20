916f74e138ed0a7ac28854e7e7f1ede5a82ab4a3
“Omprakash <omprakash.medhe@gmail.com>
2022-04-20 11:28:57 +0530

    Extent reprot modified

--- src/test/java/com/osm/APITestingFramework/listeners/ExtentManager.java
+++ src/test/java/com/osm/APITestingFramework/listeners/ExtentManager.java
@@ -16,7 +16,7 @@ public class ExtentManager {
 	        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(fileName);
 	       
 	        
-	        htmlReporter.config().setTheme(Theme.STANDARD);
+	        htmlReporter.config().setTheme(Theme.DARK);
 	        htmlReporter.config().setDocumentTitle(fileName);
 	        htmlReporter.config().setEncoding("utf-8");
 	        htmlReporter.config().setReportName(fileName);
--- src/test/java/com/osm/APITestingFramework/rough/TestIP.java
+++ src/test/java/com/osm/APITestingFramework/rough/TestIP.java
@@ -5,10 +5,10 @@ import java.net.UnknownHostException;
 
 public class TestIP {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws UnknownHostException{
 		 
 		
-		 
+		System.out.println(InetAddress.getLocalHost().getHostAddress());
 		
 		try {
 			System.out.println(InetAddress.getLocalHost().getHostAddress());
